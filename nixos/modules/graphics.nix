771aba2875cd00ffc1696557e1ec466554f186a6
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2025-05-10 12:30:09 +0200

    Fix graphics.nix

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -1,9 +1,14 @@
 # Use mesa drivers that are compatible with Hyprland for better stability
 # .. (see https://wiki.hyprland.org/Nix/Hyprland-on-NixOS/)
-{pkgs, inputs, ...}: let
+{
+  pkgs,
+  inputs,
+  ...
+}: let
   pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in {
-  hardware.graphics = {
+  hardware.opengl = {
+    enable = true;
     package = pkgs-unstable.mesa.drivers;
 
     # if you also want 32-bit support (e.g for Steam)
