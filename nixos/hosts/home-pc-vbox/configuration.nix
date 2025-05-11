ed0e26dd3d22d4132e119a6437aba352196c00cb
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2025-05-11 09:29:17 +0200

    Upgrade system state version

--- nixos/hosts/home-pc-vbox/configuration.nix
+++ nixos/hosts/home-pc-vbox/configuration.nix
@@ -63,7 +63,7 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "24.11";
 
   # Boot loader
   boot.loader.grub.enable = true;
