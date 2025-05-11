9f4d6a93efc0f00cfc4565c76382106c901f0dc8
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2025-05-11 09:36:53 +0200

    More boot hell

--- nixos/hosts/home-pc-vbox/configuration.nix
+++ nixos/hosts/home-pc-vbox/configuration.nix
@@ -67,8 +67,4 @@
 
   # Boot loader
   boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.enable = false;
 }
